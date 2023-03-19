a490000a161e5cbdfd612ee76be0a6026ff8d0e5
cc5547 <126434132+cc5547@users.noreply.github.com>
2023-03-19 12:56:25 +0900

    go

--- submission/app.py
+++ submission/app.py
@@ -42,18 +42,20 @@ def side_bar(df) :
 
   return result
 
-# 그래프 로드
-def load_graph(df_g1, df_g2):
+# 그래프 생성
+def create_graph(df_g1, df_g2):
+  fig = go.Figure()
+
+  # 그래프_1
   if df_g1 is not None:
     # 문자열에서 % 기호 제거 및 실수 타입으로 변환
-    for col in df_g1.columns[1:] : df_g1[col] = df_g1[col].apply(lambda x: float(x[:-1]))
-
-    fig = go.Figure()
+    df_g1.iloc[:, 1:] = df_g1.iloc[:, 1:].apply(lambda x: x.str.rstrip('%')).astype(float)
 
     # 연도별 색상 지정
     colors = px.colors.qualitative.Set3[:len(df_g1.columns)-1]
 
-    for i, col in enumerate(df_g1.columns[1:]) : fig.add_trace(go.Bar(x=df_g1['Unnamed: 0'], y=df_g1[col], name=col, marker_color=colors[i]))
+    for i, col in enumerate(df_g1.columns[1:]):
+        fig.add_trace(go.Bar(x=df_g1['Unnamed: 0'], y=df_g1[col], name=col, marker_color=colors[i]))
 
     # 레이아웃 설정
     fig.update_layout(
@@ -62,12 +64,12 @@ def load_graph(df_g1, df_g2):
         yaxis_title='합격률%',
         yaxis=dict(range=[0, 100]),
         plot_bgcolor='#e2f3ea', # 차트 배경색 지정
-        width = 1500,
-        height = 700,
+        width=1500,
+        height=700,
     )
 
+  # 그래프_2
   elif df_g2 is not None:
-    fig = go.Figure()
     df_g2 = df_g2.drop(df_g2.columns[1], axis=1)
     years = df_g2.columns[1:]
     colors = px.colors.qualitative.Set3[:len(years)] # 연도별 색상 리스트 생성
@@ -80,8 +82,8 @@ def load_graph(df_g1, df_g2):
         xaxis_title='시험 분류',
         yaxis_title='인원수',
         plot_bgcolor='#e2f3ea', # 차트 배경색 지정
-        width = 1500,
-        height = 700,
+        width=1500,
+        height=700,
     )
   else : pass
 
@@ -97,14 +99,13 @@ def main():
     st.title(":smile: 시험장소를 안내해드립니다 :smile:")
     st.dataframe(result, width=1000, height=500)
     st.subheader(":smile: 귀하의 합격을 기원합니다! :smile:")
-  with col2 : 
-    st.markdown("[![Foo](https://i.imgur.com/SywJPmA.png)](https://map.naver.com/)")
+  with col2 : st.markdown("[![Foo](https://i.imgur.com/SywJPmA.png)](https://map.naver.com/)")
 
   tab1, tab2 = st.tabs(['필기 년도 별 합격률' , '응시자 및 합격자 수'])
   with tab1 : 
-    st.plotly_chart(load_graph(df_g1, None))
+    st.plotly_chart(create_graph(df_g1, None))
   with tab2 : 
-    st.plotly_chart(load_graph(None, df_g2))
+    st.plotly_chart(create_graph(None, df_g2))
     
 if __name__ == '__main__':
   main()
