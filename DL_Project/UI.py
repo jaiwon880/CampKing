cf55927a64b0329fc9509f35393e3cf1c1193e63
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 15:17:22 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title="DL", layout="wide")
 def user_interface():
     st.title("뼈대 작업 중...")
 
+    # 사이드바 연동 성공 여부 
     st.subheader(True) if GetSideBar().result_sidebar() is None else st.subheader(False)
 
     image = ["https://i.imgur.com/t4O7ozH.jpg", "https://i.imgur.com/idnsDBs.gif", "https://i.imgur.com/fvRG1Tj.gif"]
