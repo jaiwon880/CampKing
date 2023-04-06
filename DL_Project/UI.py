f91b3eb193f824380e5bdf96e1630fe792dbf930
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 12:32:50 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -3,10 +3,6 @@ st.set_page_config(page_title="DL", layout="wide")
 
 def user_interface():
     st.title("뼈대 작업 중...")
-
-    # container_count = 3
-
-    
     image = ["https://i.imgur.com/t4O7ozH.jpg", "https://i.imgur.com/idnsDBs.gif", "https://i.imgur.com/fvRG1Tj.gif"]
     containers = [st.container() for i in range(len(image))]
     for i in range(len(image)) :
