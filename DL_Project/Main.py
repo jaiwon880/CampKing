4917a4ddd7342a049d4e99eedf1227532e76002c
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 16:55:26 +0900

    go

--- DL_Project/Main.py
+++ DL_Project/Main.py
@@ -1,8 +1,11 @@
 import UI
-# main 시작점
-def main() : 
-   UI.user_interface()
+from ChoiceArea import GetSideBar
 
+def main() : 
+   if GetSideBar().area_choice != "" : 
+      UI.user_interface()
+   else :
+      st.image("https://i.imgur.com/fvRG1Tj.gif")
 # 메인 실행
 if __name__ == '__main__' : 
    main()
