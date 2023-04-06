90d947296df2f2aeb3026f153e33fec5d21117f1
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 15:00:42 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -1,9 +1,10 @@
 import streamlit as st
+from ChoiceArea import GetSideBar
 st.set_page_config(page_title="DL", layout="wide")
 
 def user_interface():
     st.title("뼈대 작업 중...")
-    
+    result = GetSideBar().result_sidebar()
     image = ["https://i.imgur.com/t4O7ozH.jpg", "https://i.imgur.com/idnsDBs.gif", "https://i.imgur.com/fvRG1Tj.gif"]
     
     containers = [st.container() for i in range(len(image))]
