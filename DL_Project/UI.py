0f3ecc686cf2e1b9c8d6f58d737ba24834d81100
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 20:57:44 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -22,8 +22,32 @@ def user_interface():
     for i in range(len(image)) :
         with st.expander(f"사진_{i+1}"):
             st.image(image[i])
-    st.write(Map().result_map())
+
+
     # containers = [st.container() for i in range(len(image))]
     # for i in range(len(image)) :
     #     with containers[i] : 
     #         st.image(image[i], width = 700)
+    # =========================================================
+    # 서울 위도, 경도
+    latitude = 37.5665
+    longitude = 126.9780
+
+    # 지도 생성
+    map = folium.Map(location=[latitude, longitude], zoom_start=13)
+
+    # streamlit에 지도 출력
+    st.write(map._repr_html_(), unsafe_allow_html=True)
+
+    # 서울 시청 위도, 경도
+    city_hall_latitude = 37.5666791
+    city_hall_longitude = 126.9782914
+
+    # 마크 생성
+    marker = folium.Marker(location=[city_hall_latitude, city_hall_longitude], popup='서울 시청')
+
+    # 마크를 지도에 추가
+    marker.add_to(map)
+
+    # streamlit에 지도 출력
+    st.write(map._repr_html_(), unsafe_allow_html=True)
