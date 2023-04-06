673149ba9b971e2b44aed80a876bafbed26446f0
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 15:39:10 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -10,12 +10,12 @@ def user_interface():
 
     image = ["https://i.imgur.com/t4O7ozH.jpg", "https://i.imgur.com/idnsDBs.gif", "https://i.imgur.com/fvRG1Tj.gif"]
     
-    with st.expander("미.....원.........이....가.....2ㄱ...ㅐ......."):
-        st.image(image[0])
-
     containers = [st.container() for i in range(len(image))]
     
     for i in range(len(image)) :
-        with containers[i] : 
-            st.image(image[i], width = 700)
-    st.expander("asd")
+        with st.expander(f"사진_{i}"):
+            st.image(image[0])
+
+    # for i in range(len(image)) :
+    #     with containers[i] : 
+    #         st.image(image[i], width = 700)
