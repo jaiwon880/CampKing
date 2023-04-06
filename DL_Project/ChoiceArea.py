8f8eff7a7f8ebc6fa929d374d2e8fe505d12ccc4
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 20:25:37 +0900

    go

--- DL_Project/ChoiceArea.py
+++ DL_Project/ChoiceArea.py
@@ -12,8 +12,10 @@ class GetSideBar:
         
         self.direction_choice = self.sb.selectbox('동서남북크로스', self.direction) if self.area_choice != "" else None
 
+        self.district_choice = self.sb.selectbox('동네 입력해주세요', self.direction) if self.direction_choice != "" else None
+
     def choice_sidebar(self) : 
-        return self.direction_choice
+        return self.district_choice
         
     def result_sidebar(self) : 
         return self.choice_sidebar()
--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -16,3 +16,25 @@ class GetResult:
 class Map:
     def __init__(self) -> None:
         pass
+    # # 서울 위도, 경도
+    # latitude = 37.5665
+    # longitude = 126.9780
+
+    # # 지도 생성
+    # map = folium.Map(location=[latitude, longitude], zoom_start=13)
+
+    # # streamlit에 지도 출력
+    # st.write(map._repr_html_(), unsafe_allow_html=True)
+
+    # # 서울 시청 위도, 경도
+    # city_hall_latitude = 37.5666791
+    # city_hall_longitude = 126.9782914
+
+    # # 마크 생성
+    # marker = folium.Marker(location=[city_hall_latitude, city_hall_longitude], popup='서울 시청')
+
+    # # 마크를 지도에 추가
+    # marker.add_to(map)
+
+    # # streamlit에 지도 출력
+    # st.write(map._repr_html_(), unsafe_allow_html=True)
