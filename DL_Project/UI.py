13a72714dfb0f30377221123e8c142f497feb61a
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 15:16:49 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -4,7 +4,9 @@ st.set_page_config(page_title="DL", layout="wide")
 
 def user_interface():
     st.title("뼈대 작업 중...")
-    st.write(GetSideBar().result_sidebar())
+
+    st.subheader(True) if GetSideBar().result_sidebar() is None else st.subheader(False)
+
     image = ["https://i.imgur.com/t4O7ozH.jpg", "https://i.imgur.com/idnsDBs.gif", "https://i.imgur.com/fvRG1Tj.gif"]
     
     containers = [st.container() for i in range(len(image))]
