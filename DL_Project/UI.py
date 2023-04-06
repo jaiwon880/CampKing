ee9e69c94374e46b753c775abe66387a262f27d0
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 15:41:47 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -2,11 +2,14 @@ import streamlit as st
 from ChoiceArea import GetSideBar
 st.set_page_config(page_title="DL", layout="wide")
 
+def test() : 
+    return st.subheader(True) if GetSideBar().result_sidebar() is None else st.subheader(False)
+
 def user_interface():
     st.title("뼈대 작업 중...")
 
-    # 사이드바 연동 성공 여부 
-    st.subheader(True) if GetSideBar().result_sidebar() is None else st.subheader(False)
+    st.wrtie("연결 성공 여부는? : ", test())
+    
 
     image = ["https://i.imgur.com/t4O7ozH.jpg", "https://i.imgur.com/idnsDBs.gif", "https://i.imgur.com/fvRG1Tj.gif"]
     
