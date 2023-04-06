f3b508583aae227b3fb8c65ca377d3e724c10d49
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 20:32:42 +0900

    go

--- DL_Project/ChoiceArea.py
+++ DL_Project/ChoiceArea.py
@@ -10,12 +10,13 @@ class GetSideBar:
 
         self.area = ["", "경기도"]
         self.direction = ["", "경기 동부", "경기 서부", "경기 남부", "경기 북부"]
+        self.district = ["", "정왕동"]
 
         self.area_choice = self.sb.selectbox('지역 선택 그..치만....경기도 뿐인걸...', self.area)
         
         self.direction_choice = self.sb.selectbox('동서남북크로스', self.direction) if self.area_choice != "" else None
 
-        self.district_choice = self.sb.selectbox('동네 입력해주세요', self.direction) if self.direction_choice is not None else None
+        self.district_choice = self.sb.selectbox('동네 입력해주세요', self.district) if self.direction_choice is not None else None
 
     def choice_sidebar(self) : 
         return self.district_choice
