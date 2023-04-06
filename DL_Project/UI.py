7ccb6b6dbf27f746405cd717ba6c41fd1725a0b1
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 16:00:08 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -11,13 +11,12 @@ def user_interface():
     st.subheader(f"사이드바 연동 결과는? = {test()}")
 
     image = ["https://i.imgur.com/t4O7ozH.jpg", "https://i.imgur.com/idnsDBs.gif", "https://i.imgur.com/fvRG1Tj.gif"]
-    
-    containers = [st.container() for i in range(len(image))]
 
     for i in range(len(image)) :
         with st.expander(f"사진_{i+1}"):
             st.image(image[i])
 
+    # containers = [st.container() for i in range(len(image))]
     # for i in range(len(image)) :
     #     with containers[i] : 
     #         st.image(image[i], width = 700)
