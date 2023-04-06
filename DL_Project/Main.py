7b52d58e3a08df809f7c084360f3dc8c48f632b6
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-06 21:45:22 +0900

    go

--- DL_Project/Main.py
+++ DL_Project/Main.py
@@ -1,7 +1,5 @@
 import UI
 
-def main() : 
-   UI.user_interface()
+def main() : UI.user_interface()
    
-if __name__ == '__main__' : 
-   main()
+if __name__ == '__main__' : main()
--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -2,50 +2,31 @@ import streamlit as st
 from Functional import GetResult
 st.set_page_config(page_title="DL", layout="wide")
 
-import folium
-
-def test() : 
-    return GetResult().result_function()
+def test() : return GetResult().result_function()
 
 def user_interface():
-    # folium 지도 객체 생성
-    m = folium.Map(location=[37.566345, 126.977893], zoom_start=13)
+    st.title("뼈대 작업 중...")
+    result1, result2 = test()
+    result2 = True if result2 is None else False
 
-    # streamlit 컴포넌트에 folium 지도 렌더링
-    st.write(m._repr_html_(), unsafe_allow_html=True)
+    st.write(f"""
+        ### ChoiceArea -> Functional ->  UI 모듈 연동 결과는? = {result1}
+        ### Data -> Functional -> UI 모듈 연동 결과는? = {result2}
+    """)
 
-    # 마커 추가
-    folium.Marker(location=[37.566345, 126.977893], popup='서울특별시청').add_to(m)
+    image = [
+        "https://i.imgur.com/t4O7ozH.jpg", 
+        "https://i.imgur.com/idnsDBs.gif", 
+        "https://i.imgur.com/fvRG1Tj.gif"
+        ]
 
-    # 선 추가
-    locations = [[37.566345, 126.977893], [37.5658859, 126.9754788]]
-    folium.PolyLine(locations=locations, color='red').add_to(m)
-
-    # streamlit 컴포넌트에 folium 지도 렌더링
-    st.write(m._repr_html_(), unsafe_allow_html=True)
-
-    # st.title("뼈대 작업 중...")
-    # result1, result2 = test()
-    # result2 = True if result2 is None else False
-
-    # st.write(f"""
-    #     ### ChoiceArea -> Functional ->  UI 모듈 연동 결과는? = {result1}
-    #     ### Data -> Functional -> UI 모듈 연동 결과는? = {result2}
-    # """)
-
-    # image = [
-    #     "https://i.imgur.com/t4O7ozH.jpg", 
-    #     "https://i.imgur.com/idnsDBs.gif", 
-    #     "https://i.imgur.com/fvRG1Tj.gif"
-    #     ]
-
-    # for i in range(len(image)) :
-    #     with st.expander(f"사진_{i+1}"):
-    #         st.image(image[i])
+    for i in range(len(image)) :
+        with st.expander(f"사진_{i+1}"):
+            st.image(image[i])
 
 
-    # containers = [st.container() for i in range(len(image))]
-    # for i in range(len(image)) :
-    #     with containers[i] : 
-    #         st.image(image[i], width = 700)
+    containers = [st.container() for i in range(len(image))]
+    for i in range(len(image)) :
+        with containers[i] : 
+            st.image(image[i], width = 700)
     # =========================================================
