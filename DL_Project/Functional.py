dbf878f36d36e4814644cf0b8ac1f558e03c973b
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-07 18:19:43 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -18,7 +18,7 @@ class GetResult:
             result = self.df[(self.df['시, 군'] == self.choice)].reset_index(drop=True)
             result.index += 1 
             return result
-        if self.choice & self.address != "" :
+        if self.choice != "" and self.address != "" :
             result = self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'].str.contains(self.address))].reset_index(drop=True)
             result.index += 1 
             return result
