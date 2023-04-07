b38c4710bfcafaaace5094f9fff4030aa2609244
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-07 20:04:24 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -10,7 +10,7 @@ def user_interface():
 
     with st.sidebar :
         # 사이드바 
-        if result1 is not None : 
+        if result1 is DataFrame : 
             st.dataframe(result1)
             st.write(type(result1))
         else : pass
