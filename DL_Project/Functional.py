cfd14fd5dd751e06d7f116902b9acba4b03aeedb
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-07 18:35:49 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -5,22 +5,23 @@ class GetResult:
     def __init__(self) -> None:
         self.df = GetData().result_data()
         self.start, self.choice, self.address = GetSideBar().result_sidebar()
-        st.write(self.start)
+
     def choice_address(self) : 
         # if self.choice and self.address != "" :
         #     result = self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'].str.contains(self.address))].reset_index(drop=True)
         #     result.index += 1 
         #     return result
         # else : return None
-
         
         if self.start != "" and self.choice != "" :
             result = self.df[(self.df['시, 군'] == self.choice)].reset_index(drop=True)
             result.index += 1 
             return result
+
         if self.choice != "" and self.address != "" :
             result = self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'].str.contains(self.address))].reset_index(drop=True)
             result.index += 1 
             return result
+            
         else : return None
     def result_function(self) : return self.choice_address(), self.choice, self.address
