2dbacdeba538449b055abee607ed3865836bc505
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-07 20:54:15 +0900

    go

--- DL_Project/ChoiceArea.py
+++ DL_Project/ChoiceArea.py
@@ -1,17 +1,20 @@
 import streamlit as st
-
+from Data import GetData
+    
 class GetSideBar:
     def __init__(self) -> None:
         self.sb = st.sidebar
         self.sb.error("## 지역을 선택해주세요.")
         
-        
+        self.df = GetData().result_data()
+
         self.area = ["", "경기도"]
-        self.direction = [
-            '', '가평군', '고양시', '과천시', '광명시', '광주시', '구리시', '군포시', '김포시', '남양주시', '동두천시', 
-            '부천시', '성남시', '수원시', '시흥시', '안산시', '안성시', '안양시', '양주시', '양평군', '여주시', '연천군', 
-            '오산시', '용인시', '의왕시', '의정부시', '이천시', '파주시', '평택시', '포천시', '하남시', '화성시'
-            ]
+        self.direction = ["", self.df['시, 군'].drop_duplicates().tolist()]
+        # self.direction = [
+        #     '', '가평군', '고양시', '과천시', '광명시', '광주시', '구리시', '군포시', '김포시', '남양주시', '동두천시', 
+        #     '부천시', '성남시', '수원시', '시흥시', '안산시', '안성시', '안양시', '양주시', '양평군', '여주시', '연천군', 
+        #     '오산시', '용인시', '의왕시', '의정부시', '이천시', '파주시', '평택시', '포천시', '하남시', '화성시'
+        #     ]
 
         self.area_choice = self.sb.selectbox('지역 선택 그..치만....경기도 뿐인걸...', self.area)
         self.direction_choice = self.sb.selectbox('동서남북크로스', self.direction) if self.area_choice != "" else None
--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -5,7 +5,7 @@ class GetResult:
     def __init__(self) -> None:
         self.df = GetData().result_data()
         self.area, self.choice, self.address = GetSideBar().result_sidebar()
-        
+
     def choice_address(self) : 
         if self.choice is not None and "" : return self.plus_index(self.df[self.df['시, 군'] == self.choice].reset_index(drop=True))
         elif self.address is not None : return self.plus_index(self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'].str.contains(self.address))].reset_index(drop=True))
@@ -19,3 +19,5 @@ class GetResult:
 
     # 테스트 끝나면 위에 지울 것
     # def result_function(self) : return self.choice_address()
+
+    
