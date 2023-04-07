a729112135ccfa673ff1585c6900a7b1298ccfa5
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-07 20:16:39 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -7,8 +7,8 @@ class GetResult:
         self.area, self.choice, self.address = GetSideBar().result_sidebar()
 
     def choice_address(self) : 
-        if self.choice != None and "" : return self.plus_index(self.df[self.df['시, 군'] == self.choice].reset_index(drop=True))
-        elif self.address != None : return self.plus_index(self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'].str.contains(self.address))].reset_index(drop=True))
+        if self.choice is not None and "" : return self.plus_index(self.df[self.df['시, 군'] == self.choice].reset_index(drop=True))
+        elif self.address is not None : return self.plus_index(self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'].str.contains(self.address))].reset_index(drop=True))
         else : return None
 
     def plus_index(self, result) :
