3e307b647dbe13ef91b878c1f0dc3070fc3c7529
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-07 15:40:18 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -4,10 +4,10 @@ from Data import GetData
 class GetResult:
     def __init__(self) -> None:
         self.df = GetData().result_data()
-        self.choice, self.address = self.backend_function()
+        self.choice, self.address = GetSideBar().result_sidebar()
 
     def backend_function(self) : 
-        return GetSideBar().result_sidebar()
+        pass
 
     def result_function(self) : 
         return self.df, self.choice, self.address
