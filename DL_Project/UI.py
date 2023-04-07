84757b2a7688a782bc17d1b1dbc654a894d21ad0
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-07 20:24:41 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -9,14 +9,12 @@ def user_interface():
     st.error("## TDD - Testing...")
     result1, result2, result3, result4 = test()
 
+    # 사이드바 
     with st.sidebar :
-        # 사이드바 
-        # if result1 is not None : st.dataframe(result1)
-        # elif result1.empty : st.write("검색 결과 없음")
-        # else : pass
-        if result1 is empty : st.write("검색 결과 없음")
-        elif result1 is not None : st.dataframe(result1)
+        if result1 is not None : st.dataframe(result1)
+        elif result1 == "" : st.write("검색 결과 없음")
         else : pass
+       
     st.write(f"""
     ### 지역 선택 = {result2}
     ### 동서남북크로스 = {result3}
@@ -24,25 +22,24 @@ def user_interface():
     """)
     st.write(f"### 데이터프레임 = {result1}")
 
-    image = [
-        "https://i.imgur.com/t4O7ozH.jpg", 
-        "https://i.imgur.com/idnsDBs.gif", 
-        "https://i.imgur.com/fvRG1Tj.gif"
-        ]
+    # image = [
+    #     "https://i.imgur.com/t4O7ozH.jpg", 
+    #     "https://i.imgur.com/idnsDBs.gif", 
+    #     "https://i.imgur.com/fvRG1Tj.gif"
+    #     ]
+
+    # for i in range(len(image)) :
+    #     with st.expander(f"사진_{i+1}"):
+    #         st.image(image[i])
 
 
-    for i in range(len(image)) :
-        with st.expander(f"사진_{i+1}"):
-            st.image(image[i])
+    # containers = [st.container() for i in range(len(image))]
+    # for i in range(len(image)) :
+    #     with containers[i] : 
+    #         st.image(image[i], width = 700)
+    # # =====================================================================
+    # messages = ['success', 'info', 'warning', 'error']
 
-
-    containers = [st.container() for i in range(len(image))]
-    for i in range(len(image)) :
-        with containers[i] : 
-            st.image(image[i], width = 700)
-    # =====================================================================
-    messages = ['success', 'info', 'warning', 'error']
-
-    for i in range(2):
-        for message in messages:
-            getattr(st, message)(f'This is a {message} message')
+    # for i in range(2):
+    #     for message in messages:
+    #         getattr(st, message)(f'This is a {message} message')
