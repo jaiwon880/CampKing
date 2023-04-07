436c41d928cc1519209a201c6353e41c6c6bbb25
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-07 17:32:48 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -8,6 +8,7 @@ class GetResult:
 
     def choice_address(self) : 
         if self.choice and self.address != "":
-            return self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'] == self.address)]
-        else : return ""
+            return True
+            # return self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'] == self.address)]
+        else : return False
     def result_function(self) : return self.choice_address(), self.choice, self.address
