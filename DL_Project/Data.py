52ab3eaefad3fbece9d8bd692425687c9a0b3d63
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-07 16:41:11 +0900

    go

--- DL_Project/Data.py
+++ DL_Project/Data.py
@@ -2,16 +2,15 @@ import pandas as pd
 
 class GetData:
     def __init__(self) -> None:
-        self.df_url = True
+        self.df_url = "DL_Project/Data_csv/glamping_test.csv"
         self.df = self.load_data()
 
     def load_data(self) :
-        # try : 
-        #     return pd.read_csv(self.df_url)
-        # except Exception as e : 
-        #     return st.error(e)
-        # pass
-        return self.df_url
+        try : 
+            return pd.read_csv(self.df_url)
+        except Exception as e : 
+            return st.error(e)
+        pass
 
     def create_data(self) : 
         return self.df
