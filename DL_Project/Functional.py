7b94b1ed4f4fa36a4bce13932272c4481cc65a8b
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-07 19:27:53 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -7,7 +7,7 @@ class GetResult:
         self.area, self.choice, self.address = GetSideBar().result_sidebar()
 
     def choice_address(self) : 
-        if self.choice != "" : 
+        if self.choice != None : 
             return self.plus_index(self.df[self.df['시, 군'] == self.choice])
             
         elif self.address != None :
