66df62654dfe51df32473ec97766f1cf309c9f47
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-07 18:10:45 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -7,20 +7,20 @@ class GetResult:
         self.choice, self.address = GetSideBar().result_sidebar()
 
     def choice_address(self) : 
-        if self.choice and self.address != "" :
-            result = self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'].str.contains(self.address))].reset_index(drop=True)
-            result.index += 1 
-            return result
-        else : return None
-
-
-        # if self.choice != "" :
-        #     result = self.df[(self.df['시, 군'] == self.choice)].reset_index(drop=True)
-        #     result.index += 1 
-        #     return result
-        # elif self.choice and self.address != "" :
+        # if self.choice and self.address != "" :
         #     result = self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'].str.contains(self.address))].reset_index(drop=True)
         #     result.index += 1 
         #     return result
         # else : return None
+
+
+        if self.choice != "" :
+            result = self.df[(self.df['시, 군'] == self.choice)].reset_index(drop=True)
+            result.index += 1 
+            return result
+        elif self.choice and self.address != "" :
+            result = self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'].str.contains(self.address))].reset_index(drop=True)
+            result.index += 1 
+            return result
+        else : return None
     def result_function(self) : return self.choice_address(), self.choice, self.address
