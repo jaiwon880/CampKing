4e5622e74b791f2d51ec7c106728dfe4eca3d5f9
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-09 04:01:50 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -15,18 +15,3 @@ class GetResult:
 
     def get_result(self): 
         return self.choice_result_df(), self.area, self.choice, self.address
-
-
-    # 테스트 끝나면 위에 지울 것
-    # def get_result(self) : return self.choice_result_df()
-     # if df.empty:
-        #     columns = list(df.columns)
-        #     df.index.name = "-"
-        #     columns[:] = ["-"] * (len(columns))
-        #     columns[0] = "일치 결과 없음"
-        #     df.columns = columns
-        # else : pass
-
-    # if self.df is not None :
-    #     return self.handle_index(self.df)  
-    # else : pass
