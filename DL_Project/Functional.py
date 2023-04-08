c061825811b86e81bca05819a6f79975e87dcdf2
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-08 20:37:34 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -15,7 +15,9 @@ class GetResult:
         data = data.iloc[:, 2:].sort_values('평점', ascending=False).reset_index(drop=True)
         data.index += 1
 
-        if data.empty : data.loc[0] = ["-"] * len(data.columns)
+        if data.empty : 
+            data.loc[0, :-1] = ["-"] * (len(data.columns) - 1)
+            data.loc[0, -1] = "일치 결과 없음"
         return data
 
     def get_result(self) : return self.choice_address(), self.area, self.choice, self.address
