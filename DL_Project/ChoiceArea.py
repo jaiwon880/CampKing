49d45e729bfc866c081459af2edc7cb77e0049a9
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-08 22:28:26 +0900

    go

--- DL_Project/ChoiceArea.py
+++ DL_Project/ChoiceArea.py
@@ -11,7 +11,8 @@ class GetSideBar:
         self.area = [""] + self.df.iloc[:, 0].drop_duplicates().tolist()
         self.area_choice = self.sb.selectbox('지역 선택 그..치만....경기도 뿐인걸...', self.area)
 
-        self.direction = [""] + self.df[self.df[self.area_choice] == self.area_choice].drop_duplicates(subset='시, 군')['시, 군'].tolist() if self.area_choice != "" else None
+        self.direction = [""] + self.df[self.df[self.df.columns[0]] == self.area_choice].drop_duplicates(subset=1)[1].tolist() if self.area_choice != "" else None
+        # self.direction = [""] + self.df[self.df[self.area_choice] == self.area_choice].drop_duplicates(subset='시, 군')['시, 군'].tolist() if self.area_choice != "" else None
         self.direction_choice = self.sb.selectbox('동서남북크로스', self.direction) if self.area_choice != "" else None
         
         self.address_input = self.sb.text_input("글램핑장명 입력(동, 면이 될 수도)") if self.area_choice and self.direction_choice != "" else None
