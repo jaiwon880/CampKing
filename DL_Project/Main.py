0f5f61d09f05be6aadbf4898a1ebc64cfb38743a
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-08 20:01:01 +0900

    go

--- DL_Project/Main.py
+++ DL_Project/Main.py
@@ -1,5 +1,9 @@
+import streamlit as st
 import UI
 
-def main() : UI.user_interface()
-   
-if __name__ == '__main__' : main()
+def main() : 
+    if st.button(image="https://i.imgur.com/fvRG1Tj.gif") : 
+        UI.user_interface()
+
+if __name__ == '__main__' : 
+    main()
