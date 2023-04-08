b78a50708d7dd706dfe685be3876e7235255bd82
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-09 02:48:30 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -1,10 +1,8 @@
 import streamlit as st
-import pandas as pd
 from Functional import GetResult
 st.set_page_config(page_title="DL", layout="wide")
 
-def get() : 
-    return GetResult().get_result()
+def get() : return GetResult().get_result()
 
 def user_interface():
     st.error("## TDD - Testing...")
@@ -23,7 +21,6 @@ def user_interface():
 
     st.write(f"""
             {df}
-            
     """)
 
     # image = [
