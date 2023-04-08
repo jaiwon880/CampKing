2bf436e6291f0b1bc570643196e5a104bcfab545
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-08 22:32:09 +0900

    go

--- DL_Project/ChoiceArea.py
+++ DL_Project/ChoiceArea.py
@@ -4,7 +4,7 @@ from Data import GetData
 class GetSideBar:
     def __init__(self) -> None:
         self.sb = st.sidebar
-        self.sb.error("## 지역을 선택해주세요.")
+        self.sb.write("## 지역을 선택해주세요.")
 
         self.df = GetData().create_data()
         
@@ -12,7 +12,6 @@ class GetSideBar:
         self.area_choice = self.sb.selectbox('지역 선택 그..치만....경기도 뿐인걸...', self.area)
 
         self.direction = [""] + self.df[self.df[self.df.columns[0]] == self.area_choice].drop_duplicates(subset=self.df.columns[1])[self.df.columns[1]].sort_values().tolist() if self.area_choice != "" else None
-        # self.direction = [""] + self.df[self.df[self.area_choice] == self.area_choice].drop_duplicates(subset='시, 군')['시, 군'].tolist() if self.area_choice != "" else None
         self.direction_choice = self.sb.selectbox('동서남북크로스', self.direction) if self.area_choice != "" else None
         
         self.address_input = self.sb.text_input("글램핑장명 입력(동, 면이 될 수도)") if self.area_choice and self.direction_choice != "" else None
@@ -24,3 +23,4 @@ class GetSideBar:
         #     '부천시', '성남시', '수원시', '시흥시', '안산시', '안성시', '안양시', '양주시', '양평군', '여주시', '연천군', 
         #     '오산시', '용인시', '의왕시', '의정부시', '이천시', '파주시', '평택시', '포천시', '하남시', '화성시'
         #     ]
+    # self.direction = [""] + self.df[self.df[self.area_choice] == self.area_choice].drop_duplicates(subset='시, 군')['시, 군'].tolist() if self.area_choice != "" else None
