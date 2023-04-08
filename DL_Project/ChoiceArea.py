f15ba71d9c9d43fa14c7b051452889513b39da1d
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-09 00:10:59 +0900

    go

--- DL_Project/ChoiceArea.py
+++ DL_Project/ChoiceArea.py
@@ -5,7 +5,7 @@ class GetSideBar:
     def __init__(self) -> None:
         self.sb = st.sidebar
         self.sb.write("## 지역을 선택해주세요.")
-
+        
         self.df = self.get_data()
         
         self.area_choice = self.sb.selectbox('팔도비빔면 그..치만....경기도 뿐인걸...', self.get_area_list())
@@ -16,7 +16,7 @@ class GetSideBar:
     def get_direction_list(self) : return [""] + self.df[self.df[self.df.columns[0]] == self.area_choice]\
                                                 .drop_duplicates(subset=self.df.columns[1])[self.df.columns[1]]\
                                                 .sort_values().tolist() if self.area_choice != "" else None
-    @st.cache_data
+    
     def get_data(self) : return GetData().create_data()
     def set_data(self) : return self.df[(self.df[self.df.columns[0]] == self.area_choice)\
                                          & (self.df[self.df.columns[1]] == self.direction_choice)]\
