ba8a45a859d84bb2dda59150768dc0e2b0e0c016
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-09 02:18:12 +0900

    go

--- DL_Project/ChoiceArea.py
+++ DL_Project/ChoiceArea.py
@@ -20,8 +20,15 @@ class GetSideBar:
     def get_data(self) : return GetData().create_data()
     def set_data(self) : 
         if self.area_choice and self.direction_choice != "" :
-            return self.df[(self.df[self.df.columns[0]] == self.area_choice)\
-                    & (self.df[self.df.columns[1]] == self.direction_choice)]
-                     
+            if address_input != None :
+                return self.df[(self.df[self.df.columns[0]] == self.area_choice)\
+                                & (self.df[self.df.columns[1]] == self.direction_choice)]
+            else : pass
         else : return None
     def get_choice_result(self) : return self.area_choice, self.direction_choice, self.address_input
+
+
+    # if self.area_choice and self.direction_choice != "" :
+    #             return self.df[(self.df[self.df.columns[0]] == self.area_choice)\
+    #                     & (self.df[self.df.columns[1]] == self.direction_choice)]
+    #         else : return None
