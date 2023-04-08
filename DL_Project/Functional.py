73f922be4c49ac28a1a19b858ea6d165b06ed7d0
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-08 19:46:26 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -7,18 +7,9 @@ class GetResult:
         self.area, self.choice, self.address = GetSideBar().result_sidebar()
 
     def choice_address(self) : 
-        if self.choice :
-            filter_data = self.df[self.df['시, 군'] == self.choice]
-
-            if self.address : 
-                filter_data = filter_data[filtered_data['글램핑장'].str.contains(self.address)]
-        
-        elif self.address : 
-            filter_data = self.df[self.df['글램핑장'].str.contains(self.address)]
-        
+        if self.choice is not None and "" : return self.handle_index(self.df[self.df['시, 군'] == self.choice])
+        elif self.address is not None : return self.handle_index(self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'].str.contains(self.address))])
         else : return None
-        
-        return self.handle_index(filter_data)
 
     def handle_index(self, data):
         data = data.iloc[:, 2:].sort_values('평점', ascending=False).reset_index(drop=True)
