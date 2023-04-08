fd06d3d8fac4a94617e3bab76192c9f3bb996aab
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-08 19:22:47 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -7,17 +7,27 @@ class GetResult:
         self.area, self.choice, self.address = GetSideBar().result_sidebar()
 
     def choice_address(self) : 
-        if self.choice is not None and "" : return self.plus_index(self.df[self.df['시, 군'] == self.choice].reset_index(drop=True))
-        elif self.address is not None : return self.plus_index(self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'].str.contains(self.address))].reset_index(drop=True))
-        else : return None
+        if self.choice is not None and self.choice != "" and self.address is not None :
+            df_selected = self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'].str.contains(self.address))]
+        
+        elif self.choice is not None and self.choice != "" :
+            df_selected = self.df[self.df['시, 군'] == self.choice]
+
+        else:
+            df_selected = None
+
+        if df_selected is not None :
+            df_selected = self.plus_index(df_selected.reset_index(drop=True))
+            
+        return df_selected
+
+        # if self.choice is not None and "" : return self.plus_index(self.df[self.df['시, 군'] == self.choice].reset_index(drop=True))
+        # elif self.address is not None : return self.plus_index(self.df[(self.df['시, 군'] == self.choice) & (self.df['글램핑장'].str.contains(self.address))].reset_index(drop=True))
+        # else : return None
 
     def plus_index(self, result) :
-        if result.empty :
-            result.loc[0] = ["결과 없음"] * len(result.columns)
-        else : 
-            result.index += 1
-            
-
+        if result.empty : result.loc[0] = ["결과 없음"] * len(result.columns)
+        else : result.index += 1
         return result.iloc[:, 2:]
     
     def result_function(self) : return self.choice_address(), self.area, self.choice, self.address
