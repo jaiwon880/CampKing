e2406b4d5cd4eb58f9523b5b47d59892b271fcad
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-09 00:54:21 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -22,7 +22,7 @@ class GetResult:
             columns = list(df.columns)
             df.index.name = "-"
             columns[:] = ["-"] * (len(columns))
-            df.iloc[0, 0] = "일치 결과 없음"
+            df.loc[df.index[0], df.columns[0]] = "일치 결과 없음"
             df.columns = columns
         return df
 
