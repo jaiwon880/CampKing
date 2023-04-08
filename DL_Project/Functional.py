1bb4689371e392034512cc42170ec2ac31a5871b
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-09 01:10:15 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -17,7 +17,7 @@ class GetResult:
         #     elif self.address is not None and "" : return self.handle_index(self.df['글램핑장'].str.contains(self.address))
         #     else : return None
         # else :  None
-        return self.df
+        return handle_index(self.df)
     def handle_index(self, df):
         df = df.iloc[:, 2:].sort_values('평점', ascending=False).reset_index(drop=True)
         df.index.name = "순위"
