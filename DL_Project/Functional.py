4ff6efb15a4aa1e66f4d9d352f1b864436ad80a8
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-08 23:04:31 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -3,7 +3,7 @@ from Data import GetData
 
 class GetResult:
     def __init__(self) -> None:
-        self.df = GetSideBar().push_df()
+        self.df = GetData().result_data()
         self.area, self.choice, self.address = GetSideBar().result_sidebar()
 
     def choice_address(self) : 
