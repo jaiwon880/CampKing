3d277b020b1c80c8ae7c4669af9f030d933ff250
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-09 13:35:27 +0900

    go

--- DL_Project/Data.py
+++ DL_Project/Data.py
@@ -4,7 +4,8 @@ import streamlit as st
 @st.cache
 class GetData:
     def __init__(self):
-        self.df_url = "DL_Project/Data_csv/glamping_test.csv"
+        # self.df_url = "DL_Project/Data_csv/glamping_test.csv"
+        self.df_url = "DL_Project/Data_csv/glamping_test_3000.csv"
         self.df = self.load_data()
 
     def load_data(self):
