144fa1e3276f0131eadbf1d2bdc474ec3e71f019
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-09 23:20:03 +0900

    go

--- DL_Project/SideBar.py
+++ DL_Project/SideBar.py
@@ -7,15 +7,15 @@ class GetSideBar:
         self.sb.write("## 지역을 선택해주세요.")
         self.df = GetData().create_data()
 
-        self.area_choice = self.sb.selectbox('팔도비빔면 그..치만....경기도 뿐인걸...', self.area_list())
+        self.area_choice = self.sb.selectbox('팔도비빔면 그..치만....경기도 뿐인걸...', self.get_area_list())
         self.direction_choice = self.sb.selectbox('네넴띤선택결과', self.get_direction_list()) if self.area_choice != "" else None
         self.address_input = self.sb.text_input("글램핑장명으로 구현 중 후에(동, 면 수정)") if self.area_choice and self.direction_choice != "" else None
     
-    def area_list(self) : 
-        return list(set(df.iloc[:, 2].apply(lambda x: x.split(' ')[0] + ' ' + x.split(' ')[1])))
+    # def area_list(self) : 
+    #     return list(set(df.iloc[:, 2].apply(lambda x: x.split(' ')[0] + ' ' + x.split(' ')[1])))
 
-    # def get_area_list(self) :
-    #     return [""] + self.df.iloc[:, 0].drop_duplicates().tolist()
+    def get_area_list(self) :
+        return [""] + self.df.iloc[:, 0].drop_duplicates().tolist()
 
     def get_direction_list(self) :
         return [""] + self.df[self.df[self.df.columns[0]] == self.area_choice] \
