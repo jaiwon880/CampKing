be301b069b820c7a1ebbbaa1fd340d9caa2732bd
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 00:35:00 +0900

    go

--- DL_Project/SideBar.py
+++ DL_Project/SideBar.py
@@ -10,7 +10,7 @@ class GetSideBar:
         self.area_choice = self.sb.selectbox('팔도비빔면 그..치만....경기도 뿐인걸...', self.get_area_list())
         self.direction_choice = self.sb.selectbox('네넴띤선택결과', self.get_direction_list()) if self.area_choice != "" else None
         self.address_input = self.sb.text_input("글램핑장명으로 구현 중 후에(동, 면 수정)") if self.area_choice and self.direction_choice != "" else None
-        
+
     # def area_list(self) : 
     #     return list(set(df.iloc[:, 2].apply(lambda x: x.split(' ')[0] + ' ' + x.split(' ')[1])))
 
@@ -35,3 +35,11 @@ class GetSideBar:
 
     def get_choice_result(self):
         return self.set_data(), self.area_choice, self.direction_choice, self.address_input
+
+
+        # unique_values = list(set(df.iloc[:, 2].apply(lambda x: x.split(' ')[0])))
+        # unique_values = list(set([x.split(' ')[1] for x in df.iloc[:, 2] if x.split(' ')[0] == areachoice]))
+        # unique_values = list(set(df.iloc[:, 2].apply(lambda x: x.split(' ')[2])))
+        # unique_values = list(set(df.iloc[:, 2].apply(lambda x: x.split(' ')[1].split(' ')[0])))
+        # unique_values = list(set(df.iloc[:, 2].apply(lambda x: x.split(' ')[0] + ' ' + x.split(' ')[1])))
+
