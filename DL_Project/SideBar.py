f7b4934d82721025ecb6e4c28f75644419323025
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-09 23:19:11 +0900

    go

--- DL_Project/SideBar.py
+++ DL_Project/SideBar.py
@@ -7,7 +7,7 @@ class GetSideBar:
         self.sb.write("## 지역을 선택해주세요.")
         self.df = GetData().create_data()
 
-        self.area_choice = self.sb.selectbox('팔도비빔면 그..치만....경기도 뿐인걸...', self.get_area_list())
+        self.area_choice = self.sb.selectbox('팔도비빔면 그..치만....경기도 뿐인걸...', self.area_list())
         self.direction_choice = self.sb.selectbox('네넴띤선택결과', self.get_direction_list()) if self.area_choice != "" else None
         self.address_input = self.sb.text_input("글램핑장명으로 구현 중 후에(동, 면 수정)") if self.area_choice and self.direction_choice != "" else None
     
