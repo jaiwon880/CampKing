e7e12dc60e75208fce308a2f4b1096d038f93f46
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-09 14:00:59 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from Functional import GetResult
 
-# st.set_page_config(page_title="DL", layout="wide")
-st.set_page_config(page_title="DL")
+st.set_page_config(page_title="DL", layout="wide")
+
 def get() : return GetResult().get_result()
 
 def user_interface():
