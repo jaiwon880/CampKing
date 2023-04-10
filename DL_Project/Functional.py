a666ff757fecfe95aa56e1ed40ded7cd6abd4f47
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 18:48:27 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -14,7 +14,7 @@ class GetResult:
         # df.index = np.arange(1, len(df) + 1)
         if df is not None:
             df = df.drop_duplicates(subset=['name'], keep='first').reset_index(drop=True)
-            # df = df.iloc[:, 8:].sort_values('평점', ascending=False).reset_index(drop=True)
+            df = df.iloc[:, 8:].sort_values('ranking', ascending=False)
             df.index.name = "순위"
             df.index += 1
             return df
