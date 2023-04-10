e6dc588328d1bfcc19a91a4ddd78544d9ee16538
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 10:57:59 +0900

    go

--- DL_Project/testapp.py
+++ DL_Project/testapp.py
@@ -2,15 +2,8 @@ import streamlit as st
 import pandas as pd
 
 
-def load_data(self):
-    try :  
-        return pd.read_csv("DL_Project/Data_csv/glapingdata.csv")
-        
-    except Exception as e : 
-        return st.error(e)
-
 def main():
-    df = load_data()
+    df = pd.read_csv("DL_Project/Data_csv/glapingdata.csv")
     df = df.head()
     st.dataframe(df)
 main()
