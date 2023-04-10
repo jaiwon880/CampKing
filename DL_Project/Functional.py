c3de41e31e136b4311b6dfecf9a31d6645fef015
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 19:00:05 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -13,13 +13,22 @@ class GetResult:
         # df = df.iloc[:, 2:].sort_values('평점', ascending=False)
         # df.index = np.arange(1, len(df) + 1)
         if df is not None:
-            df = df.drop_duplicates(subset=['name'], keep='first')
-            df.sort_values(by='ranking', ascending=False, inplace=True)
-            df = df[['name', 'ranking']].reset_index(drop=True)
-            df.index.name = "순위"
-            df = df.rename(columns={'name': '업체명', 'ranking': '별점'})
-            df.index += 1
-            
+            # df = df.drop_duplicates(subset=['name'], keep='first')
+            # df.sort_values(by='ranking', ascending=False, inplace=True)
+            # df = df[['name', 'ranking']].reset_index(drop=True)
+            # df.index.name = "순위"
+            # df = df.rename(columns={'name': '업체명', 'ranking': '별점'})
+            # df.index += 1
+            df = (
+            df.drop_duplicates(subset=['name'], keep='first')
+            .sort_values(by='ranking', ascending=False)
+            [['name', 'ranking']]
+            .reset_index(drop=True)
+            .rename(columns={'name': '업체명', 'ranking': '별점'})
+            .rename_axis("순위")
+            .reset_index()
+            )
+            df['순위'] += 1
             return df
         else : return None
 
