ad773a0856cf500200b38c73717bd6a825289a84
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 20:17:42 +0900

    goo

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -27,7 +27,7 @@ def user_interface():
         with st.expander("# 메커니즘_설명 / 용량이 엄청 클 것 으로 예상 되기에 백엔드적으로도 줄여서 하기 위함. "):
             st.image("https://i.imgur.com/SgRVHOk.jpg", width = 1000)
 
-    else : st.image("https://i.imgur.com/fvRG1Tj.gif", width = 800)
+    else : st.image("https://i.imgur.com/idnsDBs.gif", width = 800)
     # image = [
     #     "https://i.imgur.com/t4O7ozH.jpg", 
     #     "https://i.imgur.com/idnsDBs.gif", 
