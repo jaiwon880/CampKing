690b89294d321fe9f390a6c00a79899625b93b9d
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 10:58:39 +0900

    go

--- DL_Project/testapp.py
+++ DL_Project/testapp.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def main():
-    df = pd.read_csv("DL_Project/Data_csv/glapingdata.csv")
+    df = pd.read_csv("DL_Project/Data_csv/glapingdata.csv", encoding="utf-8")
     df = df.head()
     st.dataframe(df)
 main()
