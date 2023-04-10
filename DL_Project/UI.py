e3fc102d209daca766da8cd19fa9ead490968c5f
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 19:47:42 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -13,9 +13,10 @@ def user_interface():
     df = get_df
     # 사이드바 
     with st.sidebar : 
-        if df is not None : 
-            st.write("일치하는 업체가 없습니다.") if df.empty else st.dataframe(df, width=700)
-        else : pass
+        if df is None : 
+            pass
+            
+        else : st.write("일치하는 업체가 없습니다.") if df.empty else st.dataframe(df, width=700)
 
     with st.expander("# 메커니즘_설명 / 용량이 엄청 클 것 으로 예상 되기에 백엔드적으로도 줄여서 하기 위함. "):
         st.image("https://i.imgur.com/SgRVHOk.jpg", width = 1000)
