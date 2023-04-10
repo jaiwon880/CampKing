2341fa638a14b3a82ce38f1a840b279c58a25444
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 19:48:39 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -18,7 +18,7 @@ def user_interface():
             
     #     else : 
     #         st.write("일치하는 업체가 없습니다.") if df.empty else st.dataframe(df, width=700)
-    st.dataframe(df)
+    if df is not None : st.dataframe(df)
     with st.expander("# 메커니즘_설명 / 용량이 엄청 클 것 으로 예상 되기에 백엔드적으로도 줄여서 하기 위함. "):
         st.image("https://i.imgur.com/SgRVHOk.jpg", width = 1000)
 
