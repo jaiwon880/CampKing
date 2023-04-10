4026b32fe2eb20679cf220a5303a83525445090b
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 20:04:33 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -16,9 +16,10 @@ def user_interface():
         if df is not None : st.write("일치하는 업체가 없습니다.") if df.empty else st.dataframe(df, width=700)
  
     with st.container():
-        st.write(f"""
-                    ### 선택한 결과 입니다. 👉{area} {direction} {address}
-        """)
+        if area and direction is not None :
+            st.write(f"""
+                        ### 선택한 결과 입니다. 👉{area} {direction} {address}
+            """)
     st.markdown("---")
     with st.expander("# 메커니즘_설명 / 용량이 엄청 클 것 으로 예상 되기에 백엔드적으로도 줄여서 하기 위함. "):
         st.image("https://i.imgur.com/SgRVHOk.jpg", width = 1000)
