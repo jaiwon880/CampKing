394110a1bd37b0ce1cce21f32cccf5ce5f723fe5
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 17:53:36 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -10,11 +10,11 @@ class GetResult:
     #     # self.audio = GetData().create_audio()
 
     def handle_df(self, df) :
-    #     # df = df.iloc[:, 2:].sort_values('평점', ascending=False)
-    #     # df.index = np.arange(1, len(df) + 1) 
-    #     df = df.iloc[:, 2:].sort_values('평점', ascending=False).reset_index(drop=True)
-    #     df.index.name = "순위"
-        df.reset_index(drop=True)
+        # df = df.iloc[:, 2:].sort_values('평점', ascending=False)
+        # df.index = np.arange(1, len(df) + 1) 
+        # df = df.iloc[:, 2:].sort_values('평점', ascending=False).reset_index(drop=True)
+        df.index.name = "순위"
+        df = df.reset_index(drop=True)
         df.index += 1
         return df
 
