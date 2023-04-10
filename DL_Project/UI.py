25c0631bad9616b84ff4e6a22206d83691280fbc
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 20:11:49 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -9,9 +9,11 @@ def get_df() : return GetResult().get_result()
 
 def user_interface():
     set_page()
-    title_message()
+    
     df, area, direction, address = get_df()
-    # 사이드바 
+
+    if df is not None : title_message()
+    
     with st.sidebar : 
         if df is not None : st.write("일치하는 업체가 없습니다.") if df.empty else st.dataframe(df, width=700)
  
