3edddb07aba557e0a3a1692a0b696215cb0d06fa
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 17:47:00 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -7,13 +7,12 @@ class GetResult:
         self.df, self.area, self.choice, self.address = GetSideBar().get_choice_result()
     #     # self.audio = GetData().create_audio()
     def handle_df(self, df) :
-        return self.df
     #     # df = df.iloc[:, 2:].sort_values('평점', ascending=False)
     #     # df.index = np.arange(1, len(df) + 1) 
     #     df = df.iloc[:, 2:].sort_values('평점', ascending=False).reset_index(drop=True)
     #     df.index.name = "순위"
-    #     df.index += 1
-    #     return df
+        df.index += 1
+        return df
 
     def choice_result_df(self): 
         return self.handle_df(self.df) if self.df is not None else None
