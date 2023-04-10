eeb4809467da084523454e4b62186690c18ef8f4
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 11:49:44 +0900

    go

--- DL_Project/SideBar.py
+++ DL_Project/SideBar.py
@@ -7,24 +7,19 @@ class GetSideBar:
         self.sb.write("## 지역을 선택해주세요.")
         self.df = GetData().create_data().drop_duplicates()
 
-        self.area_choice = self.sb.selectbox('팔도비빔면 그..치만....경기도 뿐인걸...', self.get_area_list())
-        self.direction_choice = self.sb.selectbox('네넴띤선택결과', self.get_direction_list()) if self.area_choice != "" else None
-        # self.address_input = self.sb.text_input("글램핑장명으로 구현 중 후에(동, 면 수정)") if self.area_choice and self.direction_choice != "" else None
-
+        self.area_choice = self.sb.selectbox('팔도 선택', self.get_area_list())
+        self.direction_choice = self.sb.selectbox('시, 군', self.get_direction_list()) if self.area_choice != "" else None
+        self.address_choice = self.sb.selectbox('동, 면, 군', self.get_address_list()) if self.area_choice and self.direction_choice != "" else None
+       
     def get_area_list(self) : 
         return [""] + list(set(self.df.iloc[:, 3].apply(lambda x: x.split(' ')[0])))
 
-    # def get_direction_list(self) :
-    #     # self.area_choice 변수에 저장된 값과 일치하는 세 번째 열의 값을 가진 행만 추출
-    #     filtered_df = self.df[self.df.iloc[:, 3].apply(lambda x: x.split(' ')[0]) == self.area_choice]
-
-    #     # 추출된 행에서 세 번째 열의 값을 문자열에서 공백으로 분리하여 두 번째 요소들을 중복을 제거한 리스트 생성
-    #     result_list = list(set(filtered_df.iloc[:, 3].apply(lambda x: x.split(' ')[1])))
-    #     return result_list
-
     def get_direction_list(self) :
-        return [""] + list(set(self.df[self.df.iloc[:, 3].apply(lambda x: x.split(' ')[0]) == self.area_choice].iloc[:, 3].apply(lambda x: x.split(' ')[1])))
+        return [""] + list(set(self.df[self.df.iloc[:, 3].apply(lambda x: x.split(' ')[0]) == self.area_choice]\
+                                                                .iloc[:, 3].apply(lambda x: x.split(' ')[1])))
     
+    def get_address_list(self) :
+        return self.get_direction_list()
     # def set_data(self):
     #     if self.area_choice != "" and self.direction_choice != "":
     #         if self.address_input != "" :
