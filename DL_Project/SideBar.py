a59d580a0684de169643798321a077df2eda61bc
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 12:24:40 +0900

    go

--- DL_Project/SideBar.py
+++ DL_Project/SideBar.py
@@ -9,7 +9,7 @@ class GetSideBar:
 
         self.area_choice = self.sb.selectbox('팔도 선택', self.get_area_list())
         self.direction_choice = self.sb.selectbox('시, 군', self.get_direction_list()) if self.area_choice != "" else None
-        self.address_choice = self.sb.selectbox('동, 면, 군, 읍', self.get_address_list()) if self.area_choice and self.direction_choice != "" else None
+        self.address_choice = self.sb.selectbox('구, 동, 면, 군, 읍', self.get_address_list()) if self.area_choice and self.direction_choice != "" else None
        
     def get_area_list(self) : 
         return [""] + list(set(self.df.iloc[:, 3].apply(lambda x: x.split(' ')[0])))
@@ -33,9 +33,6 @@ class GetSideBar:
                 return self.df[(self.df.iloc[:, 3].apply(lambda x: x.split(' ')[0] == self.area_choice)) \
                                 & (self.df.iloc[:, 3].apply(lambda x: x.split(' ')[1] == self.direction_choice))]
 
-                # return self.df[self.df.iloc[:, 3].apply(lambda x: x.split(' ')[0] == self.area_choice)]
-                # return self.df[(self.df[self.df.columns[0]] == self.area_choice)
-                #             & (self.df[self.df.columns[1]] == self.direction_choice)]
         else : return None
 
     # def get_choice_result(self):
