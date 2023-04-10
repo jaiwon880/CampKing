c8e796d2c07cf74fed41da42ee57ff8920dfbb96
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 23:20:07 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -13,7 +13,7 @@ def search_result(area, direction, address) : return \
 def mecanism_ment() : return "# 메커니즘_설명 / 용량이 엄청 클 것 으로 예상 되기에 메모리 최적화. "
 def mechanism_image() : return st.image("https://i.imgur.com/SgRVHOk.jpg", width = 1000)
 def start_image() : return "https://i.imgur.com/idnsDBs.gif"
-
+def image() : return ["https://i.imgur.com/t4O7ozH.jpg", "https://i.imgur.com/idnsDBs.gif", "https://i.imgur.com/fvRG1Tj.gif"]
 # def audio() : return GetResult().get_audio()
 
 def user_interface():
@@ -23,7 +23,12 @@ def user_interface():
     if df is not None : 
         title_ment(area, direction, address)
         with st.sidebar : sidebar_print_df(df)
-        with st.container() : pass
+
+        containers = [st.container() for i in range(len(image()))]
+        for i in range(len(image())) :
+            with containers[i] : 
+                st.image(image()[i], width = 700)
+        
         with st.expander(mecanism_ment()) : mechanism_image()
     else : 
         st.image(start_image(), width = 1000)
