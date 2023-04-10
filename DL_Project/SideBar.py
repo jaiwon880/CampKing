bb3be39d7ade099d805c11415e1e866ccf66e3b5
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 22:44:03 +0900

    go

--- DL_Project/SideBar.py
+++ DL_Project/SideBar.py
@@ -20,22 +20,22 @@ class GetSideBar:
 
     def get_direction_list(self) : 
         return [""] + list(set(self.df[self.split_location(0) == self.area_choice]\
-                                                .iloc[:, 3].apply(lambda x: x.split(' ')[1])))
+                                    .iloc[:, 3].apply(lambda x: x.split(' ')[1])))
     
     def get_address_list(self) : 
         return [""] + list(set(self.df[(self.split_location(0) == self.area_choice) \
-                                        & (self.split_location(1) == self.direction_choice)]\
-                                                    .iloc[:, 3].apply(lambda x: x.split(' ')[2])))
+                                    & (self.split_location(1) == self.direction_choice)]\
+                                        .iloc[:, 3].apply(lambda x: x.split(' ')[2])))
 
     def set_choice_result_data(self):
         if self.area_choice != "" and self.direction_choice != "":
             if self.address_choice != "" :
-                return self.df[(self.df.iloc[:, 3].apply(lambda x: x.split(' ')[0] == self.area_choice)) \
-                                & (self.df.iloc[:, 3].apply(lambda x: x.split(' ')[1] == self.direction_choice))\
-                                & (self.df.iloc[:, 3].apply(lambda x: x.split(' ')[2] == self.address_choice))]
+                return self.df[(self.split_location(0) == self.area_choice)\
+                            & (self.split_location(1) == self.direction_choice)\
+                            & (self.split_location(2) == self.address_choice)]
             else:
-                return self.df[(self.df.iloc[:, 3].apply(lambda x: x.split(' ')[0] == self.area_choice)) \
-                                & (self.df.iloc[:, 3].apply(lambda x: x.split(' ')[1] == self.direction_choice))]
+                return self.df[(self.split_location(0) == self.area_choice) \
+                                & (self.split_location(1) == self.direction_choice))]
         else : return None
 
     def choice_result_sidebar(self) : return self.set_choice_result_data(), self.area_choice, self.direction_choice, self.address_choice
