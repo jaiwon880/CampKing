7579715473ddae0ce035b82d73b1e36180c833ce
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 11:01:02 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -8,8 +8,8 @@ def get_df() : return GetResult().get_result()
 def user_interface():
     set_page()
     st.error("## TDD - Testing...")
-    area = get_df()
-    st.write(area)
+    
+    
     # df, area, direction, address = get_df()
 
     # # 사이드바 
