2f1bd0fe7e00528f42cf26759ce6abfd6127b51f
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-10 18:44:14 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -7,16 +7,18 @@ class GetResult:
         result_sb = GetSideBar()
         self.area, self.choice, self.address = result_sb.get_choice_result()
         self.df = result_sb.set_data()
-    #     # self.audio = GetData().create_audio()
+        # self.audio = GetData().create_audio()
 
     def handle_df(self, df) :
         # df = df.iloc[:, 2:].sort_values('평점', ascending=False)
-        # df.index = np.arange(1, len(df) + 1) 
-        df = df.drop_duplicates(subset=['name'], keep='first', inplace=True).reset_index(drop=True)
-        # df = df.iloc[:, 8:].sort_values('평점', ascending=False).reset_index(drop=True)
-        df.index.name = "순위"
-        df.index += 1
-        return df
+        # df.index = np.arange(1, len(df) + 1)
+        if df is not None:
+            df = df.drop_duplicates(subset=['name'], keep='first', inplace=True).reset_index(drop=True)
+            # df = df.iloc[:, 8:].sort_values('평점', ascending=False).reset_index(drop=True)
+            df.index.name = "순위"
+            df.index += 1
+            return df
+        else : return None
 
     def choice_result_df(self): 
         return self.handle_df(self.df) if self.df is not None else None
