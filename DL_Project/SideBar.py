94b5de26435895a68b75317f818603734b8e6d9f
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-13 01:25:59 +0900

    go

--- DL_Project/SideBar.py
+++ DL_Project/SideBar.py
@@ -8,7 +8,7 @@ class GetSideBar:
         self.df = GetData().create_data()
 
         self.area_choice = self.sb.selectbox('🌳Choice Area!🌳', self.get_area_list())
-        self.direction_choice = self.sb.selectbox('🏕시, 군🏕', self.get_direction_list()) if self.area_choice != "" else None
+        self.direction_choice = self.sb.selectbox('🏕City🏕', self.get_direction_list()) if self.area_choice != "" else None
         # self.address_choice = self.sb.selectbox('구, 동, 면, 군, 읍', self.get_address_list()) if self.area_choice and self.direction_choice != "" else None
     
 
