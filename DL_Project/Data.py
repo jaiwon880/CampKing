a41db9472db96533b8d7697042738dbdf3413a1e
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-12 19:42:38 +0900

    go

--- DL_Project/Data.py
+++ DL_Project/Data.py
@@ -6,7 +6,7 @@ import streamlit as st
 class GetData:
     def __init__(self) -> None : 
         # self.df_path = "DL_Project/Data_csv/glamping_data.csv"
-        self.df_path = "DL_Project/Data_csv/final__data.csv"
+        self.df_path = "DL_Project/Data_csv/df_final.csv"
         self.df = self.load_data()
 
     def load_data(self):
