51706b5c7f27ba7c1b59a2dd03462f045338e724
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-13 00:50:18 +0900

    go

--- DL_Project/Main.py
+++ DL_Project/Main.py
@@ -4,11 +4,11 @@ import base64
 from pydub.playback import play
 from Functional import GetResult
 
-# def audio_BGM():
-#     audio_file = open('DL_Project/Data_csv/outdoor_crackling_fire_sound.mp3', 'rb').read()
-#     return st.markdown(f'<audio autoplay loop="true" src="data:audio/mp3;base64,\
-#                         {base64.b64encode(audio_file).decode()}"></audio>',\
-#                         unsafe_allow_html=True)
+def audio_BGM():
+    audio_file = open('DL_Project/Data_csv/outdoor_crackling_fire_sound.mp3', 'rb').read()
+    return st.markdown(f'<audio autoplay loop="true" src="data:audio/mp3;base64,\
+                        {base64.b64encode(audio_file).decode()}"></audio>',\
+                        unsafe_allow_html=True)
 
 def get_search_result() : 
     return GetResult().get_result()
@@ -19,7 +19,7 @@ def sidebar_print_df(df) :
 def main() : 
     ui.set_page()
 
-    ui.audio_BGM()
+    audio_BGM()
 
     df, area, direction = get_search_result()
 
