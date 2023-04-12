57cf1f71e158b516d9b242085890dd0eee9fc868
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-13 00:46:53 +0900

    go

--- DL_Project/Main.py
+++ DL_Project/Main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import UI as ui
-import base64
-from pydub.playback import play
+# import base64
+# from pydub.playback import play
 from Functional import GetResult
 
-def audio_BGM():
-    audio_file = open('DL_Project/Data_csv/outdoor_crackling_fire_sound.mp3', 'rb').read()
-    return st.markdown(f'<audio autoplay loop="true" src="data:audio/mp3;base64,\
-                        {base64.b64encode(audio_file).decode()}"></audio>',\
-                        unsafe_allow_html=True)
+# def audio_BGM():
+#     audio_file = open('DL_Project/Data_csv/outdoor_crackling_fire_sound.mp3', 'rb').read()
+#     return st.markdown(f'<audio autoplay loop="true" src="data:audio/mp3;base64,\
+#                         {base64.b64encode(audio_file).decode()}"></audio>',\
+#                         unsafe_allow_html=True)
 
 def get_search_result() : 
     return GetResult().get_result()
