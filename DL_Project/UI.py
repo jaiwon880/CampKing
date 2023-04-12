6637e9c473a634559ac7b9034666dc0fd94216aa
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-12 19:50:42 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -26,7 +26,7 @@ def start_background() : return st.markdown("""
 
 def get_df() : return GetResult().get_result()
 
-def sidebar_print_df(df) : return st.dataframe(df, width=500)
+def sidebar_print_df(df) : return st.dataframe(df, width=300)
 
 def title_ment(area, direction) : return st.error(f"# 👉{area} {direction}"), st.markdown("---")
 
