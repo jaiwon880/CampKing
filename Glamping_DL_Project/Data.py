f855303e8189fb35bc1354fa1c47f257b72f9078
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-12 15:54:50 +0900

    go

---
+++ Glamping_DL_Project/Data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import streamlit as st
+
+@st.cache
+class GetData:
+    def __init__(self) -> None : 
+        # self.df_path = "DL_Project/Data_csv/glapingdata.csv"
+        self.df_path = "DL_Project/Data_csv/glamping_data.csv"
+        self.df = self.load_data()
+
+    def load_data(self):
+        try :  
+            return pd.read_csv(self.df_path)
+            
+        except Exception as e : 
+            return st.error(e)
+
+    def create_data(self) : return self.df
--- Glamping_DL_Project/UI.py
+++ Glamping_DL_Project/UI.py
@@ -1,4 +1,7 @@
 import streamlit as st
-
+from Data import GetData
+def get_data() : return GetData().create_data()
 def user_interface():
-    st.write("독산 서선생의 서시.....")
+    
+    st.title("독산 서선생의 서시.....")
+    st.dataframe(get_data())
