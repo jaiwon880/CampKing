12c2b438b3aa7977731fb27e0cd9b2bdc6558a69
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-12 19:39:13 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -29,22 +29,24 @@ def get_df() : return GetResult().get_result()
 def sidebar_print_df(df) : return st.dataframe(df, width=500)
 
 def title_ment(area, direction) : return st.error(f"# 👉{area} {direction}"), st.markdown("---")
-def search_result(area, direction) : return st.write(f"### 선택한 결과 입니다. ") 
 
-def mecanism_ment() : return "# 메커니즘_설명 / 용량이 엄청 클 것 으로 예상 되기에 메모리 최적화. "
-def mechanism_image() : return st.image("https://i.imgur.com/SgRVHOk.jpg", width = 1000)
+# def search_result(area, direction) : return st.write(f"### 선택한 결과 입니다. ") 
 
-def image() : return ["https://i.imgur.com/t4O7ozH.jpg", "https://i.imgur.com/idnsDBs.gif", "https://i.imgur.com/fvRG1Tj.gif"]
-def containers() : return [st.container() for i in range(len(image()))]
+# def mecanism_ment() : return "# 메커니즘_설명 / 용량이 엄청 클 것 으로 예상 되기에 메모리 최적화. "
+# def mechanism_image() : return st.image("https://i.imgur.com/SgRVHOk.jpg", width = 1000)
 
+# def image() : return ["https://i.imgur.com/t4O7ozH.jpg", "https://i.imgur.com/idnsDBs.gif", "https://i.imgur.com/fvRG1Tj.gif"]
+# def containers() : return [st.container() for i in range(len(image()))]
+    # for i in range(len(image())) :
+    #         with containers()[i] : st.image(image()[i], width = 700)
+    
 def user_interface():
     set_page()
     
     df, area, direction = get_df()
 
-    audio_file = open('DL_Project/Data_csv/outdoor_crackling_fire_sound.mp3', 'rb')
-    audio_bytes = audio_file.read()
-    st.markdown(f'<audio autoplay="true" src="data:audio/mp3;base64,{base64.b64encode(audio_bytes).decode()}"></audio>',unsafe_allow_html=True)
+    audio_file = open('DL_Project/Data_csv/outdoor_crackling_fire_sound.mp3', 'rb').read()
+    st.markdown(f'<audio autoplay="true" src="data:audio/mp3;base64,{base64.b64encode(audio_file).decode()}"></audio>',unsafe_allow_html=True)
 
     if df is not None : 
         # set_background()
@@ -53,8 +55,7 @@ def user_interface():
         # with st.expander(mecanism_ment()) : mechanism_image()
         
         
-        # for i in range(len(image())) :
-        #     with containers()[i] : st.image(image()[i], width = 700)
+        
     else : 
         start_background()
         
