ca6113061333feeb40e41e7d252084e9e18ba652
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-13 20:11:09 +0900

    해

--- DL_Project/Data.py
+++ DL_Project/Data.py
@@ -5,13 +5,27 @@ import pickle
 @st.cache
 class GetData:
     def __init__(self) -> None : 
-        self.df_path = "DL_Project/Data_csv/df_final.csv"
+        self.path = ["DL_Project/Data_csv/df_final.csv",\
+                    "DL_Project/Data_csv/total.csv",\
+                    "DL_Project/Data_csv/gapyeong.csv",\
+                    "DL_Project/Data_csv/pocheon.csv"]
+
         self.df = self.load_data()
+        self.total, self.gapyeong, self.pocheon = self.load_area_price()
 
     def load_data(self):
         try :  
-            return pd.read_csv(self.df_path)
+            return pd.read_csv(self.path[0])
         except Exception as e : 
             return st.error(e)
 
-    def create_data(self) : return self.df
+    def load_area_price(self) :
+        try:
+            return pd.read_csv(self.path[1], index_col =0, encoding="utf-8"),\
+                    pd.read_csv(self.path[2], index_col =0, encoding="utf-8"),\
+                    pd.read_csv(self.path[3], index_col =0, encoding="utf-8")
+
+        except Exception as e:
+            return st.error(e)
+
+    def create_data(self) : return self.df, self.total, self.gapyeong, self.pocheon
