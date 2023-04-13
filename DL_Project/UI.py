941e5e566a7f7158b79703d3335b71ab66e062e1
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-13 13:58:46 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -48,4 +48,9 @@ def total_image() :
             st.image("https://i.imgur.com/QGxbZJa.png")
 
 def refactoring() : 
-    return st.write("사용자 에게 도출될 키워드 리뷰, 업체 사진, 객실 정보 등은 한글 화 진행 중 추후 리팩토링..")
+    ment = "사용자 에게 도출될 키워드 리뷰 카운드(%별 수), 업체 사진(image), 객실 정보(info) 등은 한글 화 진행 중 추후 리팩토링.."
+    return st.markdown(f"<div style='background-color: white; \
+                        padding: 10px; color: green; font-size: 48px;\
+                        font-weight: bold; display: inline-block;'> \
+                        👉{ment} \
+                        </div>", unsafe_allow_html=True)
