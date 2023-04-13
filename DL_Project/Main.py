1a1f63de29ac98567da1e32eff9fec2f07c2483b
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-13 14:36:50 +0900

    go

--- DL_Project/Main.py
+++ DL_Project/Main.py
@@ -45,7 +45,7 @@ def main() :
             ui.cutting()
             ui.pocheon_load()
 
-        else : 
+        elif direction == "전체":
             ui.this_direction("전체")
             ui.cutting()
             ui.total_load()
--- DL_Project/SideBar.py
+++ DL_Project/SideBar.py
@@ -19,7 +19,7 @@ class GetSideBar:
 
     def get_direction_list(self) : 
         return [""] + sorted(list(set(self.df[self.split_location(0) == self.area_choice]\
-                                                .iloc[:, 3].apply(lambda x: x.split(' ')[1]))))
+                                                .iloc[:, 3].apply(lambda x: x.split(' ')[1]))))+["전체"]
     
     # def get_address_list(self) : 
     #     return [""] + sorted(list(set(self.df[(self.split_location(0) == self.area_choice)\
