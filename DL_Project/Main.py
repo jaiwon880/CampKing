fd037d154f377b20458e8d75306970d890966414
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-13 10:26:54 +0900

    go

--- DL_Project/Main.py
+++ DL_Project/Main.py
@@ -46,9 +46,9 @@ def main() :
         # st.write(np.sum(pocheon))
         # st.write(np.sum(total))
 
-        st.write(gapyeong.summary())
-        st.write(pocheon.summary())
-        st.write(total.summary())
+        # st.write(gapyeong.summary())
+        # st.write(pocheon.summary())
+        # st.write(total.summary())
 
 
         # containers = [st.container() for i in range(len(df.head()))]
