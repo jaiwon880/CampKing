3c512a9f78df65c5b24ddf6ca69aa5740ba1d3ed
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-16 19:22:27 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -80,7 +80,10 @@ class GetResult:
             df = df.rename(columns={"importance" : "🤜가격 산정"})
             keyword = pd.DataFrame(df["🤜가격 산정"][:11]).transpose()
 
-            fig = go.Figure(go.Bar(y=df.index, x=df["🤜가격 산정"], orientation='h'))
+            colors = ['rgb({},{},{})'.format(random.randint(0,255), random.randint(0,255), random.randint(0,255)) for i in range(len(df))]
+
+            fig = go.Figure(go.Bar(y=df.index, x=df["🤜가격 산정"], orientation='h', marker=dict(color=colors)))
+            # fig = go.Figure(go.Bar(y=df.index, x=df["🤜가격 산정"], orientation='h'))
             
             fig.update_layout(title='가격 산정 결과', xaxis_title='가격', yaxis_title='')
             fig.update_xaxes(tickformat=".0f")
