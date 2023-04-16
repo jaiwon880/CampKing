5991d5c9f254df6896a1cba8bddd226b1d30c54c
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-16 22:10:51 +0900

    go

--- DL_Project/UI.py
+++ DL_Project/UI.py
@@ -39,7 +39,7 @@ def start_background() :
                 </style> """, unsafe_allow_html=True)
 
 def title_ment(area, direction, count) : 
-    st.markdown(f"<div style='background-color:  #dde7e7; \
+    st.markdown(f"<div style='background-color:  #e8e2d6 ; \
                     padding: 10px; color: white; font-size: 48px;\
                     font-weight: bold; display: inline-block;'> \
                     👉{area} {direction} {count}곳 의 업체 결과\
