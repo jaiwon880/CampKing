3354d4b50924ff5921c2e814bb25d6dd22dd78b0
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-17 01:50:17 +0900

    go

--- DL_Project/UI_UX.py
+++ DL_Project/UI_UX.py
@@ -7,15 +7,15 @@ class User_Interface :
     def __init__(self) -> None:
         self.cutting = st.markdown("---")
 
-    def cutting() : return self.cutting
+    def cutting(self) : return self.cutting
 
-    def set_page() : 
+    def set_page(self) : 
         st.set_page_config(page_title="for Doksan Seo teacher",\
                         page_icon="🏕️", \
                         layout="wide", \
                         initial_sidebar_state="auto", \
                         )
-    def set_background():
+    def set_background(self):
     # https://t1.daumcdn.net/cfile/blog/99C6924C5B65B8BD02
     # https://i.imgur.com/PSeW0pm.gif
         st.markdown("""<style>
@@ -24,27 +24,27 @@ class User_Interface :
                             background-size: cover;
                         }
                         </style> """, unsafe_allow_html=True)
-    def start_background() : 
+    def start_background(self) : 
         st.markdown("""<style>
                     .main {
                             background-image: url('https://i.imgur.com/idnsDBs.gif');
                             background-size: cover;
                         }
                     </style> """, unsafe_allow_html=True)
-    def title_ment(area, direction, count) : 
+    def title_ment(self, area, direction, count) : 
         st.markdown(f"<div style='background-color: green; \
                         padding: 10px; color: white; font-size: 48px;\
                         font-weight: bold; display: inline-block;'> \
                         👉{area} {direction} {count}곳 의 업체 결과\
                         </div>", unsafe_allow_html=True)                
-    def refactoring_ment() : 
+    def refactoring_ment(self) : 
         ment = "업체가 10개 미만입니다. 분석에 의미가 없습니다."
         st.markdown(f"<div style='background-color: white; \
                     padding: 10px; color: green; font-size: 48px;\
                     font-weight: bold; display: inline-block;'> \
                     👉{ment} \
                     </div>", unsafe_allow_html=True)
-    def sidebar_print_df(df) :
+    def sidebar_print_df(self, df) :
         if len(df) > 10 :
             st.write("# Best!"), st.dataframe(df.head(), width=600)
             st.write("# Worst!"), st.dataframe(df.tail(), width=600)
@@ -57,7 +57,7 @@ class User_Experience :
         self.audio_path = "DL_Project/Data_csv/outdoor_crackling_fire_sound.mp3"
         self.audio = open(audio_path, 'rb').read()
 
-    def set_BGM():
+    def set_BGM(self):
         st.markdown(f'<audio autoplay loop="true" src="data:audio/mp3;base64,\
                     {base64.b64encode(self.audio).decode()}"></audio>',\
                     unsafe_allow_html = True)          
