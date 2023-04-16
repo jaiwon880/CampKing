1de3ff496aeed5c9ec2dad571bf812c76e351564
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-16 18:02:38 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -48,7 +48,7 @@ class GetResult:
         df, image_path = self.handle_price()
 
         if df is not None and image_path is not None  :
-            # df = df.rename(columns{"importance" : "🤜가격 산정"})
+            df = df.rename(columns{"importance" : "🤜가격 산정"})
             keyword = pd.DataFrame(df["importance"][:11]).transpose()
             
             fig, ax = plt.subplots(figsize=(10, 8))
