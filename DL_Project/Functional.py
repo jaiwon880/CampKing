7efb5af0b32db87483da45f5489b125361620185
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-16 23:28:01 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -53,7 +53,7 @@ class GetResult:
         
         fig.update_layout(
             title='😁 옵션 별 가격 순위표 😁', 
-            title_font=dict(size=24),
+            title_font=dict(size=24, family='Arial', color='black', weight='bold'),
 
             xaxis_title='가격', 
             xaxis_title_font_color='green',
