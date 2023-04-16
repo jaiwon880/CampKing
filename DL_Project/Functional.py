b13335de9b991d351393eadae30a491d374a54e2
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-16 19:25:23 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -84,7 +84,7 @@ class GetResult:
             fig = go.Figure(go.Bar(y=df.index, x=df["🤜가격 산정"], orientation='h', marker=dict(color=colors)))
             # fig = go.Figure(go.Bar(y=df.index, x=df["🤜가격 산정"], orientation='h'))
             
-            fig.update_layout(title='가격 산정 결과', xaxis_title='가격', yaxis_title='')
+            fig.update_layout(title='옵션 별 가격 순위표', xaxis_title='가격', yaxis_title='옵션')
             fig.update_xaxes(tickformat=".0f")
             
             st.image(image_path)
