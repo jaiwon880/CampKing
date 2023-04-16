c35857c0f83848a98af0c31be1014a33edb3b4be
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-16 20:08:58 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -40,7 +40,6 @@ class GetResult:
         return self.choice_result_df(), self.area, self.direction
 
 
-
     def handle_price(self) : 
         if self.direction == "전체" : return self.total, self.image_path[0]
         elif self.direction == "가평군" : return self.gapyeong, self.image_path[1]
@@ -49,13 +48,9 @@ class GetResult:
 
     def get_price(self):
         df, image_path = self.handle_price()
+        df = self.rename_df() if df is not None else ""
 
         if df is not None and image_path is not None:
-            df = df.rename(index={\
-                                    'name': '이름(인지도)🌞', \
-                                    'ranking' : '리뷰 별점⭐',\
-                                    'info_poolvilla' : '풀빌라🛁'
-                                    })
             df = df.rename(columns={"importance" : "🤜가격 산정"})
             keyword = pd.DataFrame(df["🤜가격 산정"][:11]).transpose()
 
@@ -70,3 +65,58 @@ class GetResult:
             st.dataframe(keyword)
         else:
             pass
+
+    def rename_df(self, df) : 
+        df = df.rename(index={\
+                "info_bogcheung": "복청숙🏠",\
+                'name': '이름(인지도)🌞',\
+                'ranking': '리뷰 별점⭐',\
+                'info_glamping': '글램핑🏕️',\
+                'roomname': '객실명🛏️',\
+                'info_caravan': '카라반🚐',\
+                'max_standard': '최대 인원수👥',\
+                'info_privatehouse': '전원주택🏡',\
+                'visitor_photo': '사진 촬영📷',\
+                'visitor_entertainment': '주변 관광지🏞️',\
+                'info_pension': '펜션🏘️',\
+                'info_bathroom': '개별 욕실🛁',\
+                'visitor_clean': '청결도🧼',\
+                'info_spa': '스파/월풀/사우나/마사지💆‍♂️',\
+                'visitor_view': '전망🌅',\
+                'info_deck': '테라스/데크🪑',\
+                'info_terrace': '테라스🌳',\
+                'people_stadard': '표준 인원수👥',\
+                'visitor_bbq': '바베큐시설🍖',\
+                'visitor_sink': '세탁기/식기세척기🧺',\
+                'info_grass': '잔디밭/초원🌿',\
+                'info_oceanview': '오션뷰/해운대뷰🌊',\
+                'visitor_maintainance': '유지보수🛠️',\
+                'visitor_kind': '친절도🙂',\
+                'visitor_rest': '휴식공간🧘‍♀️',\
+                'visitor_goodprice': '가성비👍',\
+                'info_unisex': '남녀공용화장실🚻',\
+                'visitor_parking': '주차시설🅿️',\
+                'visitor_publictransport': '대중교통🚉',\
+                'visitor_hotwater': '온수/정수기🚰',\
+                'visitor_waterplay': '수영장/물놀이시설🏊',\
+                'visitor_mannertime': '체크인/아웃 시간⏰',\
+                'visitor_bathroom': '욕실청소상태🚽',\
+                'visitor_interior': '인테리어/가구🛋️',\
+                'info_ondolroom': '온돌/한옥🏯',\
+                'homepage': '홈페이지🌐',\
+                'visitor_shade': '그늘막/텐트/파라솔☂️',\
+                'visitor_wide': '넓이/공간📏',\
+                'visitor_concept': '컨셉🎨',\
+                'visitor_child': '어린이 놀이시설',\
+                'homepage': '홈페이지🌐',\
+                'visitor_shade': '그늘막/텐트/파라솔☂️',\
+                'visitor_wide': '넓이/공간📏',\
+                'visitor_concept': '컨셉🎨',\
+                'visitor_child': '어린이 놀이시설🤸‍♀️',\
+                'visitor_temperature': '온도/냉방/난방❄️',\
+                'visitor_facility': '시설🚪',\
+                'visitor_noise': '소음🔇',\
+                'visitor_bed': '침대/이불🛏️',\
+                'visitor_pet': '애완동물🐶'\
+                                })
+        return df
