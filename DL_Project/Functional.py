bf50af9acb71cf48aaf9364c61073f12fcc11835
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-16 20:59:31 +0900

    go

--- DL_Project/Functional.py
+++ DL_Project/Functional.py
@@ -56,7 +56,7 @@ class GetResult:
                 plot_bgcolor='rgb(255, 255, 204)',
                 # paper_bgcolor='rgb(255, 255, 204)'
                 )
-            fig.update_xaxes(tickformat=",.0f원")
+            fig.update_xaxes(tickformat=",.0f")
             
             # st.image(image_path)
             st.plotly_chart(fig)
