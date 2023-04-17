d4130106f82c6fb5330464fc74cce79d3aa0a385
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-17 21:44:43 +0900

    go

--- DL_Project/UI_UX.py
+++ DL_Project/UI_UX.py
@@ -32,21 +32,18 @@ class User_Interface :
         self.split_count = 10 # 프레임 자르는 개수 기준 
         self.left_column, self.right_column = st.columns([8, 2])
 
-        self.background_set = """<style>
-                                .main {
-                                    background-image: url('https://t1.daumcdn.net/cfile/blog/99C6924C5B65B8BD02');
-                                    background-size: cover;
-                                    }
-                                    </style> """, unsafe_allow_html=True
-
-
     def choice_result(self) : return self.df, self.direction
 
     def get_col(self) : return self.left_column, self.right_column
 
     def cutting(self): return st.markdown("---")
 
-    def set_background(self) : st.markdown(self.background_set)
+    def set_background(self) : st.markdown("""<style>
+                                            .main {
+                                                background-image: url('https://t1.daumcdn.net/cfile/blog/99C6924C5B65B8BD02');
+                                                background-size: cover;
+                                                }
+                                            </style> """, unsafe_allow_html=Truet)
         
     def start_background(self): st.markdown("""<style>
                                             .main {
