8832b4be9ec301e571acd552a5945c2a65a28554
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-17 12:25:08 +0900

    go

--- DL_Project/UI_UX.py
+++ DL_Project/UI_UX.py
@@ -115,7 +115,7 @@ class User_Interface :
 
 class User_Experience(User_Interface) :
     def __init__(self) -> None:
-        super().__init__()
+        # super().__init__()
         self.audio_path = "DL_Project/Data_csv/outdoor_crackling_fire_sound.mp3"
         self.audio = open(self.audio_path, 'rb').read()
 
