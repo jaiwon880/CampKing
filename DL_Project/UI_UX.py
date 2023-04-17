beb23e3db94e3eb32e3383fc2102f56c4e53d3ba
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-17 22:03:38 +0900

    go

--- DL_Project/UI_UX.py
+++ DL_Project/UI_UX.py
@@ -99,7 +99,7 @@ class User_Interface :
             xaxis=dict(tickfont=dict(color='green'), tickfont_size = 20),
             yaxis=dict(tickfont=dict(color='green'), tickfont_size = 20),
             
-            width = 800,
+            width = 1000,
             height = 1400,
             
             plot_bgcolor='rgb(230, 245, 230)',
