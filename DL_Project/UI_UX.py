3033abe2a3c0fb130ff9959f45d0612227094256
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-18 00:08:44 +0900

    go

--- DL_Project/UI_UX.py
+++ DL_Project/UI_UX.py
@@ -109,12 +109,13 @@ class User_Interface :
         #     keywor_price = pd.DataFrame(self.price_df["🤜가격 산정"][start_idx:end_idx]).transpose().round(0).astype(int)
         #     st.dataframe(keywor_price, width = 1200)
         # ========================================================================================================================
-        keyword_price = self.price_df.sort_values(by="🤜가격 산정")[["🤜가격 산정"]].round(0).astype(int)
-        keyword_price = self.price_df.style.background_gradient(cmap='Greens', subset=pd.IndexSlice[:, self.price_df.columns[:1]])
-        st.dataframe(keyword_price, width = 400, height = 1650)
+        df = self.price_df.sort_values(by="🤜가격 산정")[["🤜가격 산정"]].round(0).astype(int)
+        # keyword_price = self.price_df.style.background_gradient(cmap='Greens', subset=pd.IndexSlice[:, self.price_df.columns[:1]])
+        df = df.style \
+                        .background_gradient(cmap='Greens', subset=pd.IndexSlice[:, df.columns[0]:]) \
+                        .background_gradient(cmap='Blues', subset=pd.IndexSlice[:, df.columns[1]:])
+        st.dataframe(df, width = 400, height = 1650)
 
-    def style_dataframe(self, df):
-        return 
 class User_Experience :
     def __init__(self) -> None:
         self.audio_path = "DL_Project/Data_csv/outdoor_crackling_fire_sound.mp3"
