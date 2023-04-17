c7b175e019f7414c3756747d853500a668b0e92a
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-17 23:40:15 +0900

    gi

--- DL_Project/UI_UX.py
+++ DL_Project/UI_UX.py
@@ -10,13 +10,7 @@ from pydub.playback import play
 from Functional import GetResult
 
 
-def set_page() : st.set_page_config(page_title="for Doksan Seo teacher",
-                                    page_icon="🏕️", 
-                                    layout="wide", 
-                                    initial_sidebar_state="auto",)
-
-
-
+def set_page() : st.set_page_config(page_title="for Doksan Seo teacher", page_icon="🏕️", layout="wide", initial_sidebar_state="auto")
 
 class User_Interface :
     def __init__(self) -> None:
@@ -108,21 +102,6 @@ class User_Interface :
         st.plotly_chart(fig)
 
     def print_price(self) :
-        # for i in range(math.ceil(len(self.price_df) / self.split_count)):
-        #     start_idx = i * self.split_count
-        #     end_idx = min(start_idx + self.split_count, len(self.price_df))
-
-        #     dfs = []
-        #     for col in self.price_df.columns:
-        #         dfs.append(self.price_df[col][start_idx:end_idx].round(0).astype(int))
-            
-        #     # 수평으로 병합하기
-        #     merged_df = pd.concat(dfs, axis=1)
-            
-        #     # 줄바꿈 무시하고 출력하기
-        #     st.dataframe(merged_df.to_string(index=True, header=False, col_space=12, line_width=100000), unsafe_allow_html=True)
-            # st.write(keywor_price.T.style.set_table_styles([{'selector': 'th', 'props': [('max-width', '50px')]}]), width=1200)
-            # st.dataframe(keywor_price, width = 1200)
         # ========================================================================================================================
         # for i in range(math.ceil(len(self.price_df)/self.split_count)):
         #     start_idx = i * self.split_count
@@ -131,13 +110,8 @@ class User_Interface :
         #     keywor_price = pd.DataFrame(self.price_df["🤜가격 산정"][start_idx:end_idx]).transpose().round(0).astype(int)
         #     st.dataframe(keywor_price, width = 1200)
         # ========================================================================================================================
-        # for i in range(0, len(self.price_df), self.split_count):
-        #     keyword_price = self.price_df.iloc[i:i+self.split_count]["🤜가격 산정"].astype(int).round(0).transpose()
-        #     st.dataframe(keyword_price, width = 500)
-        # ========================================================================================================================
         keyword_price = self.price_df.sort_values(by="🤜가격 산정")[["🤜가격 산정"]].round(0).astype(int)
         st.dataframe(keyword_price, width = 400, height = 1650)
-
 
 class User_Experience :
     def __init__(self) -> None:
