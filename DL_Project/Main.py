e891ad560f5555091708452b151e43c88c9e679e
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-17 09:16:39 +0900

    go

--- DL_Project/Main.py
+++ DL_Project/Main.py
@@ -5,21 +5,21 @@ from Functional import GetResult
 
 def main() : 
     UI_UX.set_page()
+    
+    User_Experience().set_BGM()
+
+    # get = GetResult()
+    # df, area, direction = get.choice_result()
     ui = User_Interface()
-    ux = User_Experience()
-    ux.set_BGM()
 
-    get = GetResult()
-    df, area, direction = get.choice_result()
-
-    if df is not None : 
+    if ui.choice_result_df() is not None : 
         ui.set_background()
 
         with st.sidebar : 
-            ui.sidebar_print_df(df)
+            ui.sidebar_print_df()
         
         if direction == "전체" or direction == "가평군" or direction == "포천시" :
-            ui.title_ment(area, direction, len(df))
+            ui.title_ment()
             ui.cutting()
             get.price_result()
 
--- DL_Project/UI_UX.py
+++ DL_Project/UI_UX.py
@@ -3,47 +3,48 @@ import base64
 from pydub.playback import play
 from Functional import GetResult
 
-def set_page(): 
-    st.set_page_config(page_title="for Doksan Seo teacher",
-                        page_icon="🏕️", 
-                        layout="wide", 
-                        initial_sidebar_state="auto",
-                        )
+def set_page() : st.set_page_config(page_title="for Doksan Seo teacher",
+                                    page_icon="🏕️", 
+                                    layout="wide", 
+                                    initial_sidebar_state="auto",
+                                    )
 
 class User_Interface :
     def __init__(self) -> None:
+        self.df, \
+        self.area, \
+        self.direction = GetResult().choice_result()
+
         self.cut = st.markdown("---")
-        
+        self.ment = "유지보수중"
+
+    def choice_result_df(self) : return self.df
     def cutting(self): return self.cut
 
-    def set_background(self): 
-        st.markdown("""<style>
-                    .main {
-                            background-image: url('https://t1.daumcdn.net/cfile/blog/99C6924C5B65B8BD02');
-                            background-size: cover;
-                        }
-                        </style> """, unsafe_allow_html=True)
+    def set_background(self) : st.markdown("""<style>
+                                            .main {
+                                                background-image: url('https://t1.daumcdn.net/cfile/blog/99C6924C5B65B8BD02');
+                                                background-size: cover;
+                                                }
+                                                </style> """, unsafe_allow_html=True)
         
-    def start_background(self): 
-        st.markdown("""<style>
-                    .main {
-                            background-image: url('https://i.imgur.com/idnsDBs.gif');
-                            background-size: cover;
-                        }
-                    </style> """, unsafe_allow_html=True)
+    def start_background(self): st.markdown("""<style>
+                                            .main {
+                                                background-image: url('https://i.imgur.com/idnsDBs.gif');
+                                                background-size: cover;
+                                                }
+                                            </style> """, unsafe_allow_html=True)
 
-    def title_ment(self, area, direction, count) : 
-        st.markdown(f"<div style='background-color: green; \
-                            padding: 10px; color: white; font-size: 48px;\
-                            font-weight: bold; display: inline-block;'> \
-                            👉{area} {direction} {count}곳 의 업체 결과\
-                            </div>", unsafe_allow_html=True) 
-    def refactoring_ment(self): 
-        st.markdown(f"<div style='background-color: white; \
-                        padding: 10px; color: green; font-size: 48px;\
-                        font-weight: bold; display: inline-block;'> \
-                        👉{ment} \
-                        </div>", unsafe_allow_html=True)
+    def title_ment(self, area, direction, count) : st.markdown(f"<div style='background-color: green; \
+                                                                padding: 10px; color: white; font-size: 48px;\
+                                                                font-weight: bold; display: inline-block;'> \
+                                                                👉{self.area} {self.direction} {len(self.df)}곳 의 업체 결과\
+                                                                </div>", unsafe_allow_html=True) 
+    def refactoring_ment(self) : st.markdown(f"<div style='background-color: white; \
+                                                padding: 10px; color: green; font-size: 48px;\
+                                                font-weight: bold; display: inline-block;'> \
+                                                👉{self.ment} \
+                                                </div>", unsafe_allow_html=True)
     def sidebar_print_df(self, df):
         if len(df) > 10 :
             st.write("# Best!"), st.dataframe(df.head(), width=600)
@@ -56,10 +57,10 @@ class User_Experience :
         self.audio_path = "DL_Project/Data_csv/outdoor_crackling_fire_sound.mp3"
         self.audio = open(self.audio_path, 'rb').read()
 
-    def set_BGM(self):
-        st.markdown(f'<audio autoplay loop="true" src="data:audio/mp3;base64,\
-                    {base64.b64encode(self.audio).decode()}"></audio>',\
-                    unsafe_allow_html = True)          
+    def set_BGM(self) : st.markdown(f'<audio autoplay loop="true" src="data:audio/mp3;base64,\
+                                        {base64.b64encode(self.audio).decode()}"></audio>',\
+                                        unsafe_allow_html = True)
+
 
 
 
