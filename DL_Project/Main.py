8ae3939125f95def8d66720de249160a1273c3d6
최용재 <126434132+cc5547@users.noreply.github.com>
2023-04-17 11:15:29 +0900

    go

--- DL_Project/Main.py
+++ DL_Project/Main.py
@@ -2,19 +2,21 @@ import streamlit as st
 import UI_UX
 from UI_UX import User_Interface, User_Experience
 
+def handle_ui() : return User_Interface()
+def hadle_ux() : return User_Experience()
+
 def main() : 
     UI_UX.set_page()
     
-    User_Experience().set_BGM()
+    hadle_ux().set_BGM()
 
-    ui = User_Interface()
+    ui = handle_ui()
     df, direction = ui.choice_result()
 
     if df is not None : 
         # ui.set_background()
 
-        with st.sidebar : 
-            ui.sidebar_print_df()
+        with st.sidebar : ui.sidebar_print_df()
         
         if direction == "전체" or direction == "가평군" or direction == "포천시" :
             ui.title_ment()
